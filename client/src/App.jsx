3d9369622f30f5a6ad277a90abfcc4d7cc50c557
Mohammed <arifcsidhik@gmail.com>
2024-09-25 13:35:38 +0530

    added routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import react from 'react'
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-      <h1 className='bg-slate-400'>sfdgdh</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/about' element={<About />} ></Route>
+        <Route path='/sign-in' element={<SignIn />} ></Route>
+        <Route path='/sign-up' element={<SignUp />} ></Route>
+        <Route path='/profile' element={<Profile />} ></Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+
+
+export default Profile
